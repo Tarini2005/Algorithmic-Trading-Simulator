4c5880e165c988a086f4b82408578a0e0ce9e05c
Tarini2005 <90229260+Tarini2005@users.noreply.github.com>
2025-04-06 05:44:47 -0600

    Create Main.java

---
+++ src/main/java/com/tradingbacktester/Main.java
@@ -0,0 +1,15 @@
+package com.tradingbacktester;
+
+import com.tradingbacktester.ui.TradingBacktesterApp;
+import javafx.application.Application;
+
+/**
+ * Main entry point for the Trading Backtester application.
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        Application.launch(TradingBacktesterApp.class, args);
+    }
+}
