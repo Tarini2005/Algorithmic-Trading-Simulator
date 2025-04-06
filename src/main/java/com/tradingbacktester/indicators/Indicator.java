0232f514d0669b5ba0521fb3fe9c271a601b457d
Tarini2005 <90229260+Tarini2005@users.noreply.github.com>
2025-04-06 06:03:24 -0600

    Update Indicator.java

--- src/main/java/com/tradingbacktester/indicators/Indicator.java
+++ src/main/java/com/tradingbacktester/indicators/Indicator.java
@@ -1,1 +1,36 @@
+package com.tradingbacktester.indicators;
 
+import com.tradingbacktester.model.TimeSeries;
+import java.util.List;
+
+/**
+ * Interface for technical indicators.
+ */
+public interface Indicator {
+    
+    /**
+     * Calculates the indicator values for the given time series.
+     * 
+     * @param series the time series
+     * @return a list of indicator values
+     */
+    List<Double> calculate(TimeSeries series);
+    
+    /**
+     * Gets the name of the indicator.
+     * 
+     * @return the indicator name
+     */
+    String getName();
+    
+    /**
+     * Gets the most recent value of the indicator.
+     * 
+     * @param series the time series
+     * @return the most recent value
+     */
+    default double getValue(TimeSeries series) {
+        List<Double> values = calculate(series);
+        return values.isEmpty() ? Double.NaN : values.get(values.size() - 1);
+    }
+}
